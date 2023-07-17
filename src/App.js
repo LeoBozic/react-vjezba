4ed4c85b43cea4a7ece5cb4f4e5bf4b714ccf9f3
Leo Božić <leobozic2903@gmail.com>
2023-07-17 20:25:24 +0200

    proizvoljno razdvojen od korisnika

--- src/App.js
+++ src/App.js
@@ -4,20 +4,31 @@ import Class1 from "./Components/Class1";
 import Function2 from "./Components/Function2";
 
 export default class App extends Component {
-  state = [
-    { ime: "Leo", godine: 22 },
-    { ime: "Marko", godine: 20 },
-    { ime: "Ivan", godine: 24 },
-    { proizvoljno: "Ovo je proizvoljni tekst" },
-  ];
+  state = {
+    korisnici: [
+      { ime: "Leo", godine: 22 },
+      { ime: "Marko", godine: 20 },
+      { ime: "Ivan", godine: 24 },
+    ],
+    proizvoljno: "Ovo je proizvoljni tekst",
+  };
 
   render() {
     return (
       <div>
-        <Function1 ime={this.state[0].ime} godine={this.state[0].godine} />
-        <Class1 ime={this.state[1].ime} godine={this.state[1].godine} />
-        <Function2 ime={this.state[2].ime} godine={this.state[2].godine}>
-          {this.state[3].proizvoljno}
+        <Function1
+          ime={this.state.korisnici[0].ime}
+          godine={this.state.korisnici[0].godine}
+        />
+        <Class1
+          ime={this.state.korisnici[1].ime}
+          godine={this.state.korisnici[1].godine}
+        />
+        <Function2
+          ime={this.state.korisnici[2].ime}
+          godine={this.state.korisnici[2].godine}
+        >
+          {this.state.proizvoljno}
         </Function2>
       </div>
     );
