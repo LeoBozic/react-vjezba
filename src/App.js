fefa8ce0a2ad8a1b5bd2bb65dccb8bcc87b2a954
Leo Božić <leobozic2903@gmail.com>
2023-07-17 20:15:56 +0200

    drugi dio

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import Function1 from "./Components/Function1";
 import Class1 from "./Components/Class1";
 import Function2 from "./Components/Function2";
 
-const korisnici = [
-  { ime: "Leo", godine: 22 },
-  { ime: "Marko", godine: 20 },
-  { ime: "Ivan", godine: 24 },
-];
 export default class App extends Component {
+  state = [
+    { ime: "Leo", godine: 22 },
+    { ime: "Marko", godine: 20 },
+    { ime: "Ivan", godine: 24 },
+    { proizvoljno: "Ovo je proizvoljni tekst" },
+  ];
+
   render() {
     return (
       <div>
-        <Function1 ime={korisnici[0].ime} godine={korisnici[0].godine} />
-        <Class1 ime={korisnici[1].ime} godine={korisnici[1].godine} />
-        <Function2 ime={korisnici[2].ime} godine={korisnici[2].godine}>
-          <div>Children</div>
+        <Function1 ime={this.state[0].ime} godine={this.state[0].godine} />
+        <Class1 ime={this.state[1].ime} godine={this.state[1].godine} />
+        <Function2 ime={this.state[2].ime} godine={this.state[2].godine}>
+          {this.state[3].proizvoljno}
         </Function2>
       </div>
     );
